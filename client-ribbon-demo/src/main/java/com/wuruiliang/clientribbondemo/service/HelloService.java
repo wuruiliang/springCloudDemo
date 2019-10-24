d406c476d820552493e4186a50a458ed8443ccad
ruiliangwu@yangqianguan.com <ruiliangwu@yangqianguan.com>
2019-10-24 14:59:37 +0800

    在client-ribbon-demo中添加hystrix熔断器

--- client-ribbon-demo/src/main/java/com/wuruiliang/clientribbondemo/service/HelloService.java
+++ client-ribbon-demo/src/main/java/com/wuruiliang/clientribbondemo/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.wuruiliang.clientribbondemo.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +11,13 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "error")
     public String helloService(){
         return restTemplate.getForObject("http://eureka-client/hello", String.class);
+    }
+
+    public String error(){
+        return "sorry error!";
     }
 
 }
