90e4f6fb4de042bc63eadd4837b108c39a4b78d0
ruiliangwu@yangqianguan.com <ruiliangwu@yangqianguan.com>
2019-10-28 15:42:54 +0800

    添加config-server项目作为配置中心，并在clent-feign-demo中添加config-client使用配置中心的配置项

--- client-feign-demo/src/main/java/com/wuruiliang/clientfeigndemo/controller/HelloController.java
+++ client-feign-demo/src/main/java/com/wuruiliang/clientfeigndemo/controller/HelloController.java
@@ -2,16 +2,26 @@ package com.wuruiliang.clientfeigndemo.controller;
 
 import com.wuruiliang.clientfeigndemo.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @Autowired
     HelloService helloService;
+
+    @Value("${foo}")
+    String foo;
 
     @RequestMapping("/hello")
     public String sayHello(){
         return helloService.sayHelloFromEurekaClient();
+    }
+
+    @RequestMapping("/foo")
+    public String foo(){
+        return foo;
     }
 }
---
+++ config-server/src/test/java/com/wuruiliang/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuruiliang.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
